98b99e8c726505d8ed33e25779e1aed66c77156a
= <=>
2022-06-17 17:45:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const app = express()
+
+const { Client } = require("pg")
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL
+})
+client.connect()
+
+app.get("/create-table", async(req, res) => {
+    client.query("CREATE TABLE IF NOT EXISTS users (name VARCHAR(20), email VARCHAR(20))", (error, result) => {
+        if(error){
+            return res.json(error)
+        }
+        res.json(result)
+    })
+})
+
+app.get("/insert-data", async(req, res) => {
+    client.query("INSERT INTO users (name, email) VALUES ('rajesh', 'rajesh@rajesh.com')", (error, result) => {
+        if(error){
+            return res.json(error)
+        }
+        res.json(result)
+    })
+})
+
+app.get("/read-data", async(req, res) => {
+    client.query("SELECT * FROM users", (error, result) => {
+        if(error){
+            return res.json(error)
+        }
+        res.json(result)
+    })
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Running server at port 3000...");
+})
